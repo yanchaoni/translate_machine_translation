e7e65672e9b6bb206b9819d939e770f88abca8db
yanchaoni <yn811@nyu.edu>
2018-11-30 00:40:39 -0500

    arg parser

--- main.py
+++ main.py
@@ -9,93 +9,81 @@ from tools.preprocess import *
 from train import trainIters
 
 
-"""
-Issues:
-need mask when doing attention
-"""
-data_path = "/scratch/yn811/MT_data"
-FT_emb_path = '/scratch/yn811/'
-plot_save_path = "results/"
-save_result_path = "results/"
-save_model_name = "attn"
-language = "zh"
-device = DEVICE
-print(device)
-teacher_forcing_ratio = 0.5
-max_len_ratio = 0.9
-batch_size = 64
-source_words_to_load = 1000000
-target_words_to_load = 1000000
-encoder_hidden_size = 150
-decoder_hidden_size = 150
-encoder_layers = 1
-decoder_layers = 1
-plot_every = 100000
-teacher_forcing_ratio = 0.5
-learning_rate = 0.001
-n_iters = 200
-beam_width=10
-min_len=5
-n_best=5
-use_bi = True
-decode_method = "beam"
-decoder_type = "attn"
+def main(args):
+    if args.decoder_type == "attn":
+        args.use_bi = True
+    source_words_to_load = 1000000
+    target_words_to_load = 1000000
+    input_lang, output_lang, train_pairs, train_max_length = prepareData("train", args.language, "en", args.data_path, args.max_len_ratio=args.max_len_ratio)
+    _, _, dev_pairs, _ = prepareData('dev', args.language, 'en', path=args.data_path, args.max_len_ratio=0.99999)
+    # _, _, test_pairs, _ = prepareData('test', args.language, 'en', path=args.data_path)
+    if args.language == "zh":
+        file_check(args.FT_emb_path+'chinese_ft_300.txt')
+        source_embedding, source_notPretrained = load_fasttext_embd(args.FT_emb_path+'chinese_ft_300.txt', input_lang, input_lang, source_words_to_load)
+    else:
+        file_check(args.FT_emb_path+'vietnamese_ft_300.txt')
+        source_embedding, source_notPretrained = load_fasttext_embd(args.FT_emb_path+'vietnamese_ft_300.txt', input_lang, input_lang, source_words_to_load)
+
+    file_check(args.FT_emb_path+'english_ft_300.txt')
+    target_embedding, target_notPretrained = load_fasttext_embd(args.FT_emb_path+'english_ft_300.txt', output_lang, input_lang, target_words_to_load)
+
+    params = {'args.batch_size':args.batch_size, 'shuffle':False, 'collate_fn':vocab_collate_func, 'num_workers':20}
+    params2 = {'args.batch_size':args.batch_size, 'shuffle':False, 'collate_fn':vocab_collate_func, 'num_workers':20}
+
+    train_set, dev_set = Dataset(train_pairs, input_lang, output_lang), Dataset(dev_pairs, input_lang, output_lang)
+    train_loader = torch.utils.data.DataLoader(train_set, **params)
+    dev_loader = torch.utils.data.DataLoader(dev_set, **params2)
+
+    encoder = EncoderRNN(input_lang.n_words, EMB_DIM, args.encoder_hidden_size,
+                        args.encoder_layers, args.decoder_hidden_size, source_embedding, args.use_bi, args.device).to(args.device)
+    if args.decoder_type == "basic":
+        decoder = DecoderRNN(output_lang.n_words, EMB_DIM, args.decoder_hidden_size,
+                            args.decoder_layers, target_embedding, dropout_p=0.1, args.device=args.device).to(args.device)
+    elif args.decoder_type == "attn":
+        decoder = DecoderRNN_Attention(output_lang.n_words, EMB_DIM, args.decoder_hidden_size,
+                                    args.decoder_layers, target_embedding, dropout_p=0.1, args.device=args.device).to(args.device)
+    else:
+        raise ValueError
+
+    print(encoder, decoder)
+    trainIters(encoder, decoder, train_loader, dev_loader, \
+                input_lang, output_lang, train_max_length, \
+                args.epoch, args.plot_every=args.plot_every, \
+                args.learning_rate=args.learning_rate, args.device=args.device, args.teacher_forcing_ratio=args.teacher_forcing_ratio, label=args.save_model_name,
+                use_lr_scheduler = True, gamma_en = 0.99, gamma_de = 0.99, 
+                args.beam_width=args.beam_width, args.min_len=args.min_len, args.n_best=args.n_best, args.decode_method=args.decode_method, args.save_result_path = args.save_result_path)
+
+    showPlot(plot_losses, 'Train_Loss_Curve', args.save_result_path)
+    #encoder.load_state_dict(torch.load("encoder.pth"))
+    #decoder.load_state_dict(torch.load("attn_decoder.pth"))
 
 
-input_lang, output_lang, train_pairs, train_max_length = prepareData("train", language, "en", data_path, max_len_ratio=max_len_ratio)
-_, _, dev_pairs, _ = prepareData('dev', language, 'en', path=data_path, max_len_ratio=0.99999)
-# _, _, test_pairs, _ = prepareData('test', language, 'en', path=data_path)
-if language == "zh":
-    file_check(FT_emb_path+'chinese_ft_300.txt')
-    source_embedding, source_notPretrained = load_fasttext_embd(FT_emb_path+'chinese_ft_300.txt', input_lang, input_lang, source_words_to_load)
-else:
-    file_check(FT_emb_path+'vietnamese_ft_300.txt')
-    source_embedding, source_notPretrained = load_fasttext_embd(FT_emb_path+'vietnamese_ft_300.txt', input_lang, input_lang, source_words_to_load)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='training')
+    parser.add_argument('--language', type=str, action='store', help='source language')
+    parser.add_argument('--save_model_name', type=str, action='store', help='what name to save the model')
+    parser.add_argument('--FT_emb_path', type=str, action='store', help='what path is pretrained embedding saved/to be saved')
+    parser.add_argument('--data_path', type=str, action='store', help='what path is translation data saved')
+    parser.add_argument('--device', type=str, action='store', help='use cuda?', default=DEVICE)
+    parser.add_argument('--model_path', required=False, help='path to save model', default='./') # not imp
+    parser.add_argument('--epoch', type=int, action='store', help='number of epoches to train', default=50)
+    parser.add_argument('--batch_size', type=int, action='store', help='batch size', default=64)
+    parser.add_argument('--decoder_type', type=str, action='store', help='basic/attn', default='attn')
+    parser.add_argument('--decode_method', type=str, action='store', help='beam/greedy', default='beam')
+    parser.add_argument('--use_bi', type=bool, action='store', help='if use bid encoder', default=False)
+    parser.add_argument('--n_best', type=int, action='store', help='find >=n best from beam', default=5)
+    parser.add_argument('--min_len', type=int, action='store', help='placeholder, meaningless', default=5)
+    parser.add_argument('--beam_width', type=int, action='store', help='beam width', default=10)
+    parser.add_argument('--learning_rate', type=float, action='store', help='learning rate', default=0.001)
+    parser.add_argument('--teacher_forcing_ratio', type=float, action='store', help='teacher forcing ratio', default=0.5)
+    parser.add_argument('--plot_every', type=int, action='store', help='save plot log every ? steps', default=1e5)
+    parser.add_argument('--decoder_layers', type=int, action='store', help='num of decoder layers', default=1) # init not imp
+    parser.add_argument('--encoder_layers', type=int, action='store', help='num of encoder layers', default=1) # might have bug
+    parser.add_argument('--decoder_hidden_size', type=int, action='store', help='decoder num hidden', default=150)
+    parser.add_argument('--encoder_hidden_size', type=int, action='store', help='encoder num hidden', default=150)
+    parser.add_argument('--max_len_ratio', type=float, action='store', help='max len ratio to filter training pairs', default=0.9)
+    parser.add_argument('--save_result_path', type=str, action='store', help='what path to save results', default='results/')
 
-file_check(FT_emb_path+'english_ft_300.txt')
-target_embedding, target_notPretrained = load_fasttext_embd(FT_emb_path+'english_ft_300.txt', output_lang, input_lang, target_words_to_load)
-
-params = {'batch_size':batch_size, 'shuffle':False, 'collate_fn':vocab_collate_func, 'num_workers':20}
-params2 = {'batch_size':batch_size, 'shuffle':False, 'collate_fn':vocab_collate_func, 'num_workers':20}
-
-train_set, dev_set = Dataset(train_pairs, input_lang, output_lang), Dataset(dev_pairs, input_lang, output_lang)
-train_loader = torch.utils.data.DataLoader(train_set, **params)
-dev_loader = torch.utils.data.DataLoader(dev_set, **params2)
-
-encoder = EncoderRNN(input_lang.n_words, EMB_DIM, encoder_hidden_size,
-                     encoder_layers, decoder_hidden_size, source_embedding, use_bi, device).to(device)
-if decoder_type == "basic":
-    decoder = DecoderRNN(output_lang.n_words, EMB_DIM, decoder_hidden_size,
-                         decoder_layers, target_embedding, dropout_p=0.1, device=device).to(device)
-elif decoder_type == "attn":
-    decoder = DecoderRNN_Attention(output_lang.n_words, EMB_DIM, decoder_hidden_size,
-                                   decoder_layers, target_embedding, dropout_p=0.1, device=device).to(device)
-else:
-    raise ValueError
-
-print(encoder, decoder)
-trainIters(encoder, decoder, train_loader, dev_loader, \
-            input_lang, output_lang, train_max_length, \
-            n_iters, plot_every=plot_every, \
-            learning_rate=learning_rate, device=device, teacher_forcing_ratio=teacher_forcing_ratio, label=save_model_name,
-            use_lr_scheduler = True, gamma_en = 0.99, gamma_de = 0.99, 
-            beam_width=beam_width, min_len=min_len, n_best=n_best, decode_method=decode_method, save_result_path = save_result_path)
-
-showPlot(plot_losses, 'Train_Loss_Curve', plot_save_path)
-#encoder.load_state_dict(torch.load("encoder.pth"))
-#decoder.load_state_dict(torch.load("attn_decoder.pth"))
-
-
-# if __name__ == '__main__':
-#     parser = argparse.ArgumentParser(description='training')
-#     parser.add_argument('--device', type=str, action='store', help='use cuda?', default='cuda')
-#     parser.add_argument('--model_path', required=False, help='path to save model', default='./checkpoint')
-#     parser.add_argument('--epoch', type=int, action='store', help='number of epoches to train', default=50)
-#     parser.add_argument('--batch_size', type=int, action='store', help='batch size', default=32)
-#     parser.add_argument('--model', type=str, action='store', help='model to use', default='vanilla')
-#     parser.add_argument('--resume', type=str, action='store', help='model path to resume', default='False')
-#     parser.add_argument('--dataset', type=str, action='store', help='dataset to train on', default="../data/fl_processed_data_train.pickle")
-
-#     args = parser.parse_args()
-#     print(args)
-#     main(args)
+    args = parser.parse_args()
+    print(args)
+    main(args)
