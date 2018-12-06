05103eb38ce89ea8a9ca92c9df0f45af777a6a2e
Leah Wang <406080563@qq.com>
2018-12-06 03:17:05 -0500

    add layer norm

--- models/encoder_decoder.py
+++ models/encoder_decoder.py
@@ -438,3 +438,19 @@ class Maxout(nn.Module):
         out = self.lin(inputs)
         m, i = out.view(*shape).max(max_dim)
         return m
+
+class LayerNorm(nn.Module):
+    "Construct a layernorm module (See citation for details)."
+    "Thanks to https://arxiv.org/abs/1607.06450 and http://nlp.seas.harvard.edu/2018/04/03/attention.html#model-architecture"
+    
+    def __init__(self, features, eps=1e-6):
+        super(LayerNorm, self).__init__()
+        self.a_2 = nn.Parameter(torch.ones(features))
+        self.b_2 = nn.Parameter(torch.zeros(features))
+        self.eps = eps
+
+    def forward(self, x):
+        mean = x.mean(-1, keepdim=True)
+        std = x.std(-1, keepdim=True)
+        return self.a_2 * (x - mean) / (std + self.eps) + self.b_2
+
