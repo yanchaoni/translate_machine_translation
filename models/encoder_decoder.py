66eef3eed3a64d722d8c45b3f5b58ca56b3d591d
Leah Wang <406080563@qq.com>
2018-12-06 03:26:12 -0500

    add encoder class for stacking

--- models/encoder_decoder.py
+++ models/encoder_decoder.py
@@ -177,6 +177,18 @@ class SelfAttentionEncoderLayer(nn.Module):
 
         return x
 
+class SelfAttentionEncoder(nn.Module):
+    "Core encoder is a stack of N layers"
+    def __init__(self, layer, N):
+        super(SelfAttentionEncoder, self).__init__()
+        self.layers = clones(layer, N)
+        self.norm = LayerNorm(layer.size)
+        
+    def forward(self, x, mask):
+        "Pass the input (and mask) through each layer in turn."
+        for layer in self.layers:
+            x = layer(x, mask)
+        return self.norm(x)
 
 
 
