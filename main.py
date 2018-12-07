b2e190c22ea785338183099944bee5c4c0428b22
Xinyue Zhang <xz2139@nyu.edu>
2018-12-07 17:28:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ def main(args):
     print(len(train_loader), len(dev_loader))
     if args.self_attn:
         
-        encoder = EncoderRNN_SlefAttn(input_lang.n_words, EMB_DIM, args.encoder_hidden_size,
+        encoder = EncoderRNN_SelfAttn(input_lang.n_words, EMB_DIM, args.encoder_hidden_size,
                              args.encoder_layers, args.decoder_layers, args.decoder_hidden_size, 
                              source_embedding, source_notPretrained,
                              args.use_bi, args.device, args.self_attn, 
